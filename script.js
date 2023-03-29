8a589c1d1079db5c26bf58de44b6046937a12e4f
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-03-29 15:31:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
+// function threeSum(arr, target) {
+// // write your code here
+// 	arr.sort((a,b)=>a-b);
+
+// 	let potetialSum;
+// 	let minDiff=Number.MAX_VALUE;
+// 	for(let i=0; i<=arr.length-3 ; i++){
+// 		let sum =arr[i]+arr[i+1]+arr[i+2];
+// 		let dif=Math.abs(sum - target);
+// 		if(dif<minDiff)
+// 		{
+// 			minDiff=dif;
+// 			potetialSum = sum;
+// 		}
+// 	}
+// 	return potetialSum;
+  
+// }
+
+// module.exports = threeSum;
+
+
+
+
+
 function threeSum(arr, target) {
 // write your code here
-	arr.sort((a,b)=>a-b);
-
-	let potetialSum;
-	let minDiff=Number.MAX_VALUE;
-	for(let i=0; i<=arr.length-3 ; i++){
-		let sum =arr[i]+arr[i+1]+arr[i+2];
-		let dif=Math.abs(sum - target);
-		if(dif<minDiff)
-		{
-			minDiff=dif;
-			potetialSum = sum;
-		}
-	}
-	return potetialSum;
   
+  arr.sort((a, b) => a - b);
+  let potetialSum;
+	let minDiff=Number.MAX_VALUE;
+  for (let i = 0; i <=arr.length - 3; i++) {
+    let k = arr.length - 1;
+	  
+    while (i+1 < k)
+		{
+      let sum = arr[i] + arr[i+1] + arr[k];
+		let dif=Math.abs(sum - target);
+      if (dif < minDiff)
+	  {
+		  minDiff=dif;
+        potetialSum = sum;
+      }
+     k--;
+    }
+	  
+  }
+  return potetialSum;
 }
-
-module.exports = threeSum;
+ module.exports = threeSum;
