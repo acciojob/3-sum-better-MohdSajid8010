fcf6aef19af53631b39419f4bf4cad866dfd76fc
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-03-29 15:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort((a,b)=>a-b);
+
+	let potetialSum;
+	let minDiff=Number.MAX_VALUE;
+	for(let i=0; i<=arr.length-3 ; i++){
+		let sum =arr[i]+arr[i+1]+arr[i+2];
+		let dif=Math.abs(sum - target);
+		if(dif<minDiff)
+		{
+			minDiff=dif;
+			potetialSum = sum;
+		}
+	}
+	return potetialSum;
   
 }
 
